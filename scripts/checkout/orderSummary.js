e3174905dd28e24bab859b78533d67ba50db3510
soundar <soundar2356@gmail.com>
2024-03-13 15:37:21 +0000

    make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -4,12 +4,7 @@ import { formatCurrency } from '../utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions,getDeliveryOption} from '../../data/deliveryOptions.js';
-
-hello();
-
-const today = dayjs();
-const deliverydate = today .add(7,'days');
-console.log(deliverydate.format('dddd, MMMM D'));
+import { renderPaymentSummary } from './paymentSummary.js';
 
 export function renderOrderSummary(){
 
@@ -129,6 +124,7 @@ export function renderOrderSummary(){
   `.js-cart-item-container-${productId}`
   );
   container.remove();
+  renderPaymentSummary();
   });
 
   });
@@ -139,6 +135,7 @@ export function renderOrderSummary(){
         
         updateDeliveryOption(productId,deliveryOptionId);
         renderOrderSummary();
+        renderPaymentSummary();
         });
       });
 }     
